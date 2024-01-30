5241545ae4f07cb08a8998ec433a675f6c1d29fe
Yoav Assulin <yoav@paytweed.com>
2024-01-30 13:14:02 +0200

    feat: refactor tweed connector to calss

--- src/tweedConnector.ts
+++ src/tweedConnector.ts
@@ -2,125 +2,154 @@ import { Network, TweedClient } from "@paytweed/core-js";
 import { EthereumProvider } from "@paytweed/core-js/lib/Eip1193Provider";
 import { createConnector } from "@wagmi/core";
 
-export const getTweedConnector = async () => {
-  let provider: EthereumProvider | undefined;
-  const client = await TweedClient.create("YOUR-APP-ID", {
-    chains: [Network.ETHEREUM, Network.ETHEREUM_SEPOLIA],
-  });
+export class TweedConnector {
+  private client: TweedClient | null = null;
+  private provider: EthereumProvider | undefined;
+  constructor(appId: string, chains: Network[]) {
+    this.initialize(appId, chains);
+  }
 
-  const ensureConnected = async () => {
-    const isAuthorized = client.isAuthenticated;
-    if (!isAuthorized) await client.connect();
-  };
+  private async initialize(appId: string, chains: Network[]) {
+    this.client = await TweedClient.create(appId, { chains });
+  }
 
-  const initializeProvider = async (chainId: number) => {
-    if (!provider) {
-      provider = await client.getEthereumProvider(chainId);
+  private async ensureConnected() {
+    const isAuthorized = this.client?.isAuthenticated;
+    if (!isAuthorized) await this.client?.connect();
+  }
+
+  private async initializeProvider(chainId: number) {
+    if (!this.provider) {
+      this.provider = await this.client?.getEthereumProvider(chainId);
     }
-  };
+  }
 
-  const tweedConnector = createConnector<EthereumProvider>((config) => ({
-    id: "tweed",
-    name: "Tweed",
-    icon: "https://paytweed-assets.s3.amazonaws.com/logo-square-black.png",
-    type: "injected",
+  createConnector() {
+    return createConnector<EthereumProvider>((config) => ({
+      id: "tweed",
+      name: "Tweed",
+      icon: "https://paytweed-assets.s3.amazonaws.com/logo-square-black.png",
+      type: "injected",
 
-    async connect() {
-      await ensureConnected();
-      const defaultChainId = config.chains.at(0)?.id || 0;
-      await initializeProvider(defaultChainId);
-      const accounts = await provider?.request({
-        method: "eth_accounts",
-      });
+      connect: async () => {
+        await this.ensureConnected();
+        const defaultChainId = config.chains.at(0)?.id || 0;
+        await this.initializeProvider(defaultChainId);
 
-      const chainId = await provider?.request({
-        method: "eth_chainId",
-      });
-      const normalizedChainId = normalizeChainId(chainId);
-      config.emitter.emit("change", { chainId: normalizedChainId, accounts });
+        const accounts = await this.provider?.request({
+          method: "eth_accounts",
+        });
 
-      return { chainId: normalizedChainId, accounts };
-    },
-    async disconnect() {
-      client.logout();
-    },
-    async getAccounts() {
-      await ensureConnected();
+        const chainId = await this.provider?.request({
+          method: "eth_chainId",
+        });
 
-      const defaultChainId = config.chains.at(0)?.id || 0;
-      await initializeProvider(defaultChainId);
+        const normalizedChainId = this.normalizeChainId(chainId);
 
-      const accounts = await provider?.request({
-        method: "eth_accounts",
-      });
+        config.emitter.emit("change", { chainId: normalizedChainId, accounts });
 
-      return accounts;
-    },
-    async getChainId() {
-      await ensureConnected();
+        return { chainId: normalizedChainId, accounts };
+      },
 
-      const defaultChainId = config.chains.at(0)?.id || 0;
-      await initializeProvider(defaultChainId);
+      disconnect: async () => {
+        this.client?.logout();
+      },
 
-      const chainId = await provider?.request({
-        method: "eth_chainId",
-      });
+      getAccounts: async () => {
+        await this.ensureConnected();
 
-      return chainId;
-    },
-    async getProvider(params: any) {
-      if (!provider) {
-        const { chainId } = params;
-        await client.connect();
-        return (provider = await client.getEthereumProvider(chainId));
-      }
-      return provider;
-    },
-    async switchChain({ chainId }) {
-      const chain = config.chains.find((x) => x.id === chainId);
-      if (!chain) throw new Error(`Chain ${chainId} not found`);
+        const defaultChainId = config.chains.at(0)?.id || 0;
+        await this.initializeProvider(defaultChainId);
 
-      provider = await client.getEthereumProvider(chainId);
-      const newChainId = await provider.request({
-        method: "eth_chainId",
-      });
-      const normalizedNewChainId = normalizeChainId(newChainId);
-      if (chain.id === normalizedNewChainId)
+        const accounts = await this.provider?.request({
+          method: "eth_accounts",
+        });
+
+        return accounts;
+      },
+
+      getChainId: async () => {
+        await this.ensureConnected();
+
+        const defaultChainId = config.chains.at(0)?.id || 0;
+        await this.initializeProvider(defaultChainId);
+
+        const chainId = await this.provider?.request({
+          method: "eth_chainId",
+        });
+
+        return chainId;
+      },
+
+      getProvider: async (
+        params: { chainId?: number | undefined } | undefined
+      ): Promise<EthereumProvider> => {
+        if (!this.provider) {
+          await this.client?.connect();
+          if (!params?.chainId) throw new Error("chainId is required");
+          this.provider = await this.client?.getEthereumProvider(
+            params?.chainId
+          );
+        }
+        if (!this.provider) throw new Error("Provider not found");
+        return this.provider;
+      },
+
+      switchChain: async ({ chainId }) => {
+        const chain = config.chains.find((x) => x.id === chainId);
+        if (!chain) throw new Error(`Chain ${chainId} not found`);
+
+        this.provider = await this.client?.getEthereumProvider(chainId);
+        const newChainId = await this.provider?.request({
+          method: "eth_chainId",
+        });
+        const normalizedNewChainId = this.normalizeChainId(newChainId);
+
+        if (chain.id === normalizedNewChainId)
+          config.emitter.emit("change", { chainId });
+
+        return chain;
+      },
+
+      isAuthorized: async () => {
+        if (!this.client) return false;
+        const isAuthorized = this.client?.isAuthenticated;
+        return isAuthorized;
+      },
+
+      onAccountsChanged: async (accounts: any) => {
+        config.emitter.emit("change", { accounts });
+      },
+
+      onChainChanged: async (chain) => {
+        const chainId = this.normalizeChainId(chain);
         config.emitter.emit("change", { chainId });
+      },
 
-      return chain;
-    },
-    async isAuthorized() {
-      const isAuthorized = client.isAuthenticated;
+      onDisconnect: async () => {
+        config.emitter.emit("disconnect");
+      },
+    }));
+  }
 
-      return isAuthorized;
-    },
-    async onAccountsChanged(accounts: any) {
-      config.emitter.emit("change", { accounts });
-    },
-    async onChainChanged(chain) {
-      const chainId = normalizeChainId(chain);
-      config.emitter.emit("change", { chainId });
-    },
+  normalizeChainId(chainId: any) {
+    if (typeof chainId === "string") {
+      return Number.parseInt(
+        chainId,
+        chainId.trim().substring(0, 2) === "0x" ? 16 : 10
+      );
+    }
 
-    async onDisconnect() {
-      config.emitter.emit("disconnect");
-    },
-  }));
+    if (typeof chainId === "bigint") {
+      return Number(chainId);
+    }
 
-  return tweedConnector;
-};
+    if (typeof chainId === "number") {
+      return chainId;
+    }
 
-const normalizeChainId = (chainId: bigint | number | string | unknown) => {
-  if (typeof chainId === "string")
-    return Number.parseInt(
-      chainId,
-      chainId.trim().substring(0, 2) === "0x" ? 16 : 10
+    throw new Error(
+      `Cannot normalize chainId "${chainId}" of type "${typeof chainId}"`
     );
-  if (typeof chainId === "bigint") return Number(chainId);
-  if (typeof chainId === "number") return chainId;
-
-  throw new Error(
-    `Cannot normalize chainId "${chainId}" of type "${typeof chainId}"`
-  );
-};
+  }
+}
--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,19 +1,23 @@
-import { createConfig, http } from 'wagmi'
-import { mainnet, sepolia } from 'wagmi/chains'
-import { getTweedConnector } from './tweedConnector'
+import { Network } from "@paytweed/core-js";
+import { createConfig, http } from "wagmi";
+import { mainnet, sepolia } from "wagmi/chains";
+import { TweedConnector } from "./tweedConnector";
+
+const appId = "YOUR-APP-ID";
+const chains = [Network.ETHEREUM, Network.ETHEREUM_SEPOLIA];
 
 export const config = createConfig({
   chains: [mainnet, sepolia],
-  connectors: [await getTweedConnector()],
+  connectors: [new TweedConnector(appId, chains).createConnector()],
 
   transports: {
     [mainnet.id]: http(),
     [sepolia.id]: http(),
   },
-})
+});
 
-declare module 'wagmi' {
+declare module "wagmi" {
   interface Register {
-    config: typeof config
+    config: typeof config;
   }
 }
